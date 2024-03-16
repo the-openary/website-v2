f3e89adf6441982d6131eac0218e72108e150ac4
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-03-16 16:59:10 -0400

    add mappings for h1, a, and img

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,33 @@
+import {
+    Anchor,
+    Title,
+    AnchorProps,
+    TitleProps,
+    Image,
+    ImageProps,
+} from "@mantine/core";
 import type { MDXComponents } from "mdx/types";
+import { default as NextImage } from "next/image";
+import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
+        h1: (props) => <Title order={1} {...(props as TitleProps)}></Title>,
+        a: (props) => (
+            <Anchor
+                href={props.href as string}
+                {...(props as AnchorProps)}
+                component={Link}
+            />
+        ),
+        img: (props) => (
+            <Image
+                {...(props as ImageProps)}
+                alt={props.alt as string}
+                component={NextImage}
+            ></Image>
+        ),
+
         ...components,
     };
 }
