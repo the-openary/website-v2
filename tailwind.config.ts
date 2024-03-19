ddde05715c23a36b095f79ade917b0b046066b0d
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-03-19 00:22:25 -0400

    Add screen size indicator utility component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ export default {
                 sans: ["var(--font-sans)", ...fontFamily.sans],
             },
         },
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
     },
     plugins: [],
     darkMode: ["selector", '[data-mantine-color-scheme="dark"]'],
