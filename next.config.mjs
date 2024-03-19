2a4c2d5e5edec55509e87ebf32223b7bd84a4bde
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-03-19 01:00:30 -0400

    Add webpack copyplugin
    the md parsing logic does not work in prod because prod is built differently,
    This commit adds a build step to copy the src directory to the build directory

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,31 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// await import("./src/env.mjs");
 import remarkToc from "remark-toc";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 import remarkMdx from "remark-mdx";
 import { remarkMdxToc } from "remark-mdx-toc";
 import withMDX from "@next/mdx";
+import CopyPlugin from "copy-webpack-plugin";
 
 /** @type {import("next").NextConfig} */
 const config = {
     env: {},
     compress: true,
     pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            config.plugins.push(
+                new CopyPlugin({
+                    patterns: [{ from: "./src/", to: "./src/" }],
+                }),
+            );
+        }
+
+        return config;
+    },
 };
 
 export default withMDX({
